66413b036d91962601b54727a871029ff0ed3fc4
Michael Badeen <Michael.Badeen@autozone.com>
2019-07-01 15:16:46 -0400

    Renamed globals.css to base.css.

--- src/features/App/App.tsx
+++ src/features/App/App.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import Home from '../Pages/Home'
-import '../../styles/globals.css'
+import '../../styles/base.css'
 
-const App = (): JSX.Element => (
+const App: React.FC = (): JSX.Element => (
   <Router history={createBrowserHistory()}>
     <Switch>
       <Route exact path="/" component={Home} />
