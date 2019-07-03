7d6c71d99005205595def94111c0ffdacb104ee7
Michael Badeen <Michael.Badeen@autozone.com>
2019-07-03 16:31:56 -0400

    Fix linter issue

--- src/features/Pages/Menu/Menu.tsx
+++ src/features/Pages/Menu/Menu.tsx
@@ -9,8 +9,8 @@ const Menu: React.FC = (): JSX.Element => (
     <article className={styles.content}>
       <h2>How it Works</h2>
       <p>
-        How it works stuff goes here, I need a really long paragraph so I'm going to sit here and try and try and try
-        and try to type about nothing to fill up empty space for design purposes and such and so forth..
+        How it works stuff goes here, I need a really long paragraph so I&#39;m going to sit here and try and try and
+        try and try to type about nothing to fill up empty space for design purposes and such and so forth..
       </p>
     </article>
     <Legend />
