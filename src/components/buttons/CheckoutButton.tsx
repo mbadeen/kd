99a8b7d77e39310c17114765aa73ec7b89dc633a
Michael Badeen <Michael.Badeen@autozone.com>
2019-09-06 16:40:32 -0400

    Fix path

--- src/components/buttons/CheckoutButton.tsx
+++ src/components/buttons/CheckoutButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import ShoppingCartSVG from '../../components/svg/ShoppingCartSVG'
+import ShoppingCartSVG from '../svg/ShoppingCartSVG'
 import styles from './CheckoutButton.css'
 import { State } from '../../redux/state'
 
